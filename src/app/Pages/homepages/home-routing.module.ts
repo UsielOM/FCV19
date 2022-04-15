04ae31bbddd3bb7d29ea96f489c1496e3821d545
UsielOM <usiel31197@outlook.es>
2022-04-15 11:36:52 -0500

    Configuracion de rutas

--- src/app/Pages/homepages/home-routing.module.ts
+++ src/app/Pages/homepages/home-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from 'src/app/Components/HomeComponent/login/login.component';
 import { RegistroComponent } from 'src/app/Components/HomeComponent/registro/registro.component';
 import { UserBegingPageComponent } from '../user-beging-page/user-beging-page.component';
+import { HomepagesComponent } from './homepages.component';
 
 const routes: Routes = [
+
+  {path: '', component:HomepagesComponent,
+children:[ 
+  
+   {path: '', component:LoginComponent},
+
   {path: "registro", component:RegistroComponent},
-  {path: 'user', component:UserBegingPageComponent}
+
+   {path: 'user', component:UserBegingPageComponent}
+  ]
+}
+
 ];
 
 @NgModule({
