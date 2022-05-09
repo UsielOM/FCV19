43d7c4950ccc1412c47a8b47c968efb54a072ca1
UsielOM <usiel31197@outlook.es>
2022-05-09 15:47:04 -0500

    Servicio para las preguntas

--- src/app/service/encuesta-basica.service.ts
+++ src/app/service/encuesta-basica.service.ts
@@ -4,12 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EncuestaBasicaService {
-  private preguntasBasicas: any [] = [
+  private preguntasBasicas: preguntasBasica [] = [
     {
-
+    pregunta: "En las ultimas 24/7 horas. ¿Ha convivido con alguna persona enferma o sospechosa  de COVID-19?",
+    si:"SI",
+    no: "No"
+    },
+    {
+      pregunta: "En las ultimas 24/7 horas. ¿Le han practicado pruebas de laboratorio para diagnosticar COVID-19?",
+      si: "Si",
+      no: "No"
     }
   ]
   constructor() { }
+
+  getPreguntas():preguntasBasica[]{
+return this.preguntasBasicas;
+  }
 }
 
 export interface preguntasBasica{
