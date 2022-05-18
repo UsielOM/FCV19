5f6a413207d8c43cf1aed5aa10898fcdf247ee09
UsielOM <usiel31197@outlook.es>
2022-05-18 12:20:34 -0500

    Creacion de ruta para los signos vitales

--- src/app/Pages/user-beging-page/user-routing.module.ts
+++ src/app/Pages/user-beging-page/user-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EncuestabasicaComponent } from 'src/app/Components/UserComponent/encuestabasica/encuestabasica.component';
 import { PerfiluserComponent } from 'src/app/Components/UserComponent/perfiluser/perfiluser.component';
 import { SidbarUserComponent } from 'src/app/Components/UserComponent/sidbar-user/sidbar-user.component';
+import { SignosVitalesComponent } from 'src/app/Components/UserComponent/signos-vitales/signos-vitales.component';
 import { UserBegingPageComponent } from './user-beging-page.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
 children: [
   {path: '', component: SidbarUserComponent},
   {path: 'perfil',component:PerfiluserComponent},
-  {path: 'encuesta', component:EncuestabasicaComponent}
+  {path: 'encuesta', component:EncuestabasicaComponent},
+  {path: 'signos', component:SignosVitalesComponent}
   
  
 ]}
